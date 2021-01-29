ceb996bf77fb352280dffc39b8392a21cdda0f86
gusttavogomes <30482020+gusttavogomes@users.noreply.github.com>
2021-01-29 10:36:26 -0300

    Codigo ao final da primeira aula

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,43 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import db from '../db.json';
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  * {
+    box-sizing: border-box;
+  }
+  body { 
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
+    /* New styles */
+    display: flex;
+    flex-direction: column;
+    font-family: 'Lato', sans-serif;
+    // Deixa branco no começo
+    color: ${({ theme }) => theme.colors.contrastText};
+  }
+  html, body {
+    min-height: 100vh;
+  }
+  #__next {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
   }
 `
 
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
+const theme = db.theme;
+/**https://coolors.co/1c110a-2e0d02-f40000-940000-630000
+ * https://colorhunt.co/palette/220437
+ * https://colorhunt.co/palette/225760
+ * https://color.adobe.com/pt/create/color-wheel
+ */
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
+      
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
